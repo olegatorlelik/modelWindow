310b05b2018db179f719640fa400983e52c03c6a
olegatorlelik <lelik.belokonevvv@gmail.com>
2020-09-06 21:19:48 +0300

    valid

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const sendBtn = document.querySelector("#sendBtn");
 const closeBtn = document.querySelector("#btnClose");
 const windowModel = document.querySelector("#model");
 const windowContent = document.querySelector(".model__content");
-const formGender = document.querySelector(".radio");
+const formGender = document.querySelectorAll(".radio input");
 const radioGenderMen = document.querySelector("#genderMen");
 const radioGenderWomen = document.querySelector("#genderWomen");
 const errorText = document.querySelector("#erorClass");
-let genderValue = "";
 
+console.log(formGender);
 sendBtn.addEventListener("click", openModel);
 closeBtn.addEventListener("click", closeModel);
+name.addEventListener("input", validName);
+age.addEventListener("input", validAge);
 
 function openModel() {
   if (name.value === "") {
@@ -31,11 +33,30 @@ function closeModel() {
 }
 
 function getValueRadio() {
-  for (let elem of formGender) {
+  let saveValueGender;
+  [...formGender].forEach(function (elem) {
     if (elem.checked) {
-      genderValue = elem.value;
-      break;
+      saveValueGender = elem.value;
     }
+  });
+  return saveValueGender;
+}
+
+function validName() {
+  name.classList.remove("error-text");
+  name.classList.remove("no-error-text");
+  if (Number(name.value)) {
+    name.classList.add("error-text");
+  } else if (!Number(mame.value)) {
+    name.classList.add("no-error-text");
   }
-  return genderValue;
+}
+function validAge() {
+  age.classList.remove("error-text");
+  age.classList.remove("no-error-text");
+  if (Number(age.value)) {
+    age.classList.add("no-error-text");
+  } else if (!Number(age.value)) {
+    age.classList.add("error-text");
+  }
 }
